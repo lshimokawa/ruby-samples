97aa11ad0c0c9ec3998d4f6c697bbd0ba6c30225
Lennon Shimokawa <lshimokawa@gmail.com>
2011-08-30 10:55:08 -0500

    test_should_return_sum_with_n_numbers_comma_separated

--- string-calculator/test/calculator_test.rb
+++ string-calculator/test/calculator_test.rb
@@ -16,13 +16,17 @@ class CalculatorTest < Test::Unit::TestCase
     assert_equal @calculator.add(""), 0
   end
 
-  def test_should_return_number_on_number
+  def test_should_return_number_with_number
     assert_equal @calculator.add("1"), 1
     assert_equal @calculator.add("5"), 5
   end
 
-  def test_should_return_sum_on_two_numbers_comma_separated
-    assert_equal @calculator.add("2, 3"), 5
+  def test_should_return_sum_with_two_numbers_comma_separated
+    assert_equal @calculator.add("2,3"), 5
+  end
+
+  def test_should_return_sum_with_n_numbers_comma_separated
+    assert_equal @calculator.add("2,3,4"), 9
   end
 
 end
